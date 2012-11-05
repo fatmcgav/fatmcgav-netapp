d78cbe74966bef0d897f6cecb2ec0319f119de67
fatmcgav <gavin.williams@weareact.com>
2012-11-05 14:23:29 +0000

    Added initial fact list for system-info.

--- lib/puppet/util/network_device/netapp/facts.rb
+++ lib/puppet/util/network_device/netapp/facts.rb
@@ -10,7 +10,30 @@ class Puppet::Util::NetworkDevice::Netapp::Facts
   
   def retreive
     
+    # Create empty array
     @facts = {}
+    
+    # Invoke "system-get-info" call to gather system information. 
+    result = @transport.invoke("system-get-info")
+    
+    # Pull out system-info subset. 
+    sys_info = result.child_get("system-info")
+    
+    [ 'system-name',
+      'system-id',
+      'system-model',
+      'system-machine-type',
+      'system-serial-number',
+      'partner-system-id',
+      'partner-serial-number',
+      'system-revision',
+      'number-of-processors',
+      'memory-size',
+      ].each do |key|
+        @facts[key] = sys_info.child_get_string("#{key}".to_s)
+    end
+      
+    # Return array to calling class. 
     @facts
     
   end
