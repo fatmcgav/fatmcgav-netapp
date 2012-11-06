9fe6b14cd6f8fe6d5a3157250aa7d279e7cf05fd
fatmcgav <gavin.williams@weareact.com>
2012-11-06 17:35:59 +0000

    Updated resource def.

--- lib/puppet/provider/netapp_volume/netapp_volume.rb
+++ lib/puppet/provider/netapp_volume/netapp_volume.rb
@@ -7,26 +7,26 @@ Puppet::Type.type(:netapp_volume).provide(:netapp_volume, :parent => Puppet::Pro
   defaultfor :feature => :posix
 
   def create
-    Puppet.debug("Puppet::Provider::Netapp_volume: creating Netapp Volume #{resource[:name]} of initial size #{resource[:initsize]}")
-    result = transport.invoke("volume-create", "volume", resource[:name], "size", resource[:initsize], "containing-aggr-name", resource[:aggr], "space-reserve", resource[:spaceresv])
+    Puppet.debug("Puppet::Provider::Netapp_volume: creating Netapp Volume #{@resource[:name]} of initial size #{@resource[:initsize]}")
+    result = transport.invoke("volume-create", "volume", @resource[:name], "size", @resource[:initsize], "containing-aggr-name", @resource[:aggr], "space-reserve", @resource[:spaceresv])
     if(result.results_status == "failed")
-      Puppet.debug("Puppet::Provider::Netapp_volume: Volume #{resource[:name]} creation failed due to #{result.result_reason}. \n")
-      raise Puppet::Error, "Puppet::Device::Netapp Volume #{resource[:name]} creation failed due to #{result.result_reason} \n."
+      Puppet.debug("Puppet::Provider::Netapp_volume: Volume #{@resource[:name]} creation failed due to #{result.result_reason}. \n")
+      raise Puppet::Error, "Puppet::Device::Netapp Volume #{@resource[:name]} creation failed due to #{result.result_reason} \n."
       return false
     else 
-      Puppet.debug("Puppet::Provider::Netapp_volume: Volume #{resource[:name]} created successfully. \n")
+      Puppet.debug("Puppet::Provider::Netapp_volume: Volume #{@resource[:name]} created successfully. \n")
       return true
     end
   end
   
   def destroy
-    Puppet.debug("Puppet::Provider::Netapp_volume: destroying Netapp Volume #{resource[:name]}")
-    #transport[wsdl].delete_node_address(resource[:name])
+    Puppet.debug("Puppet::Provider::Netapp_volume: destroying Netapp Volume #{@resource[:name]}")
+    #transport[wsdl].delete_node_address(@resource[:name])
   end
 
   def exists?
-    Puppet.debug("Puppet::Provider::Netapp_volume: checking existance of Netapp Volume #{resource[:name]}")
-    result = transport.invoke("volume-list-info", "volume", resource[:name])
+    Puppet.debug("Puppet::Provider::Netapp_volume: checking existance of Netapp Volume #{@resource[:name]}")
+    result = transport.invoke("volume-list-info", "volume", @resource[:name])
     Puppet.debug("Puppet::Provider::Netapp_volume: Vol Info: " + result.sprintf() + "\n")
     if(result.results_status == "failed")
       Puppet.debug("Puppet::Provider::Netapp_volume: Volume doesn't currently exist. \n")
