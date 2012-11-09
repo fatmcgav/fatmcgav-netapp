0066aa2e71902f071199ab8debae6f79c42e9566
fatmcgav <gavin.williams@weareact.com>
2012-11-09 12:27:33 +0000

    Add type and provider for NFS export management.

---
+++ lib/puppet/provider/netapp_export/netapp_export.rb
@@ -0,0 +1,69 @@
+require 'puppet/provider/netapp'
+
+Puppet::Type.type(:netapp_export).provide(:netapp_export, :parent => Puppet::Provider::Netapp) do
+  @doc = "Manage Netapp export creation, modification and deletion."
+  
+  confine :feature => :posix
+  defaultfor :feature => :posix
+
+  def create
+    Puppet.debug("Puppet::Provider::Netapp_export: creating Netapp export rule #{@resource[:name]} on path #{@resource[:path]}.")
+    # Query Netapp to create export against path. . 
+    result = transport.invoke("nfs-exportfs-append-rules-2", "persistent", @resource[:persistent], "rules", "pathname", @resource[:path])
+    # Check result status. 
+    if(result.results_status == "failed")
+      Puppet.debug("Puppet::Provider::Netapp_export: export rule #{@resource[:name]} creation failed due to #{result.results_reason}. \n")
+      raise Puppet::Error, "Puppet::Device::Netapp export rule #{@resource[:name]} creation failed due to #{result.results_reason} \n."
+      return false
+    else 
+      Puppet.debug("Puppet::Provider::Netapp_export: export rule #{@resource[:name]} created successfully on path #{@resource[:path]}. \n")
+      return true
+    end
+  end
+  
+  def destroy
+    Puppet.debug("Puppet::Provider::Netapp_export: destroying Netapp export rule #{@resource[:name]} against path #{@resource[:path]}")
+    # Query Netapp to remove export against path. 
+    result = transport.invoke("nfs-exportfs-delete-rules", "pathnames", "pathname", @resource[:name], "persistent", @resource[:persistent])
+    # Check result returned. 
+    if(result.results_status == "failed")
+      Puppet.debug("Puppet::Provider::Netapp_export: export rule #{@resource[:name]} wasn't destroyed due to #{destroy_result.results_reason}. \n")
+      raise Puppet::Error, "Puppet::Device::Netapp export rule #{@resource[:name]} destroy failed due to #{destroy_result.results_reason} \n."
+      return false
+    else 
+      Puppet.debug("Puppet::Provider::Netapp_export: export rule #{@resource[:name]} destroyed successfully. \n")
+      return true
+    end
+  end
+
+  def exists?
+    Puppet.debug("Puppet::Provider::Netapp_export: checking existance of Netapp export rule #{@resource[:name]} against path #{@resource[:path]}")
+    # Query Netapp for export-list against path. 
+    result = transport.invoke("nfs-exportfs-list-rules-2", "pathname", @resource[:path])
+    # Check result status. 
+    if(result.results_status == "failed")
+      Puppet.debug("Puppet::Provider::Netapp_export: nfs-exportfs-list-rules-2 failed due to #{result.results_reason}. \n")
+      raise Puppet::Error, "Puppet::Device::Netapp nfs-exportfs-list-rules-2 failed due to #{result.results_reason} \n."
+      return false
+    else 
+      # Get a list of exports
+      rule_list = result.child_get("rules")
+      rules = rule_list.children_get
+      # Itterate through each 'export-info' block. 
+      rules.each do |rule|
+        # Check if the export name tag matches the resource name we're validating. 
+        if(rule.child_get_string("pathname") == @resource[:name])
+          # Match found, return true. 
+          Puppet.debug("Puppet::Provider::Netapp_export: Matching export rule exists. \n")
+          return true
+        end
+      end
+      
+      # No match found, therefore doesn't exist. Return false.  
+      Puppet.debug("Puppet::Provider::Netapp_export: Matching export rule doesn't exist. \n")
+      return false
+    end
+
+  end
+  
+end
---
+++ lib/puppet/type/netapp_export.rb
@@ -0,0 +1,35 @@
+Puppet::Type.newtype(:netapp_export) do 
+  @doc = "Manage Netapp NFS Export creation, modification and deletion."
+  
+  apply_to_device
+  
+  ensurable do
+    desc "Netapp NFS Export resource state. Valid values are: present, absent."
+    
+    defaultto(:present)
+    
+    newvalue(:present) do 
+      provider.create
+    end
+    
+    newvalue(:absent) do 
+      provider.destroy
+    end
+  end
+  
+  newparam(:name) do
+    desc "The export name."
+    isnamevar
+  end
+
+  newparam(:persistent) do
+    desc "Persistent export?"
+    newvalues(:true, :false)
+    defaultto(:true)
+  end
+  
+  newparam(:path) do
+    desc "The filer path to export."
+  end
+  
+end
