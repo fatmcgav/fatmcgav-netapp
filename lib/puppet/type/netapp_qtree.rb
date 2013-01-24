e02ff2a341b926bb69ffb2e2b38f55bb7efcd44f
fatmcgav <gavin.williams@weareact.com>
2013-01-24 17:09:47 +0000

    (#5) Updated netapp_qtree type with input validation for rspec-tests.

--- lib/puppet/type/netapp_qtree.rb
+++ lib/puppet/type/netapp_qtree.rb
@@ -20,10 +20,20 @@ Puppet::Type.newtype(:netapp_qtree) do
   newparam(:name) do
     desc "The qtree name."
     isnamevar
+    validate do |value|
+      unless value =~ /^\w+$/
+         raise ArgumentError, "%s is not a valid qtree name." % value
+      end
+    end
   end
 
   newparam(:volume) do
-    desc "The volume to create qtree against."     
+    desc "The volume to create qtree against."
+    validate do |value|
+      unless value =~ /^\w+$/
+         raise ArgumentError, "%s is not a valid volume name." % value
+      end
+    end   
   end
   
 end
