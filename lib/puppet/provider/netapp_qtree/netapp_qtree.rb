d67ffd7c852a14caba59c72a43f3da1053ac26c3
fatmcgav <gavin.williams@weareact.com>
2013-03-01 21:59:35 +0000

    Updated Qtree provider to handle missing volume.

--- lib/puppet/provider/netapp_qtree/netapp_qtree.rb
+++ lib/puppet/provider/netapp_qtree/netapp_qtree.rb
@@ -27,6 +27,11 @@ Puppet::Type.type(:netapp_qtree).provide(:netapp_qtree, :parent => Puppet::Provi
     result = transport.invoke("qtree-delete", "qtree", @resource[:name])
     # Check result returned. 
     if(result.results_status == "failed")
+      # Handle where a volume has been deleted. 
+      if result.results_errno == '13040'
+        Puppet.debug("Puppet::Provider::Netapp_qtree: qtree-delete failed due to #{result.results_reason}, which means the qtree no longer exists aswell.")
+        return true
+      end
       Puppet.debug("Puppet::Provider::Netapp_qtree: qtree #{@resource[:name]} wasn't destroyed due to #{destroy_result.results_reason}. \n")
       raise Puppet::Error, "Puppet::Device::Netapp qtree #{@resource[:name]} destroy failed due to #{destroy_result.results_reason} \n."
       return false
@@ -42,6 +47,11 @@ Puppet::Type.type(:netapp_qtree).provide(:netapp_qtree, :parent => Puppet::Provi
     result = transport.invoke("qtree-list", "volume", @resource[:volume])
     # Check result status. 
     if(result.results_status == "failed")
+      # Check required resource state
+      if @resource[:state] == 'absent' && result.results_errno == '13040'
+        Puppet.debug("Puppet::Provider::Netapp_qtree: qtree-list failed due to #{result.results_reason}, however required resource state is absent. Therefore return true.")
+        return true
+      end
       Puppet.debug("Puppet::Provider::Netapp_qtree: qtree-list failed due to #{result.results_reason}. \n")
       raise Puppet::Error, "Puppet::Device::Netapp Qtree-list failed due to #{result.results_reason} \n."
       return false
