749fad43971039575464d29e188ff2b975c6d3a2
fatmcgav <gavin.williams@weareact.com>
2013-03-06 14:02:14 +0000

    (#12) Updated netapp_export type/provider to support setting of NFS Security.

--- lib/puppet/type/netapp_export.rb
+++ lib/puppet/type/netapp_export.rb
@@ -17,13 +17,12 @@ Puppet::Type.newtype(:netapp_export) do
     end
   end
   
-  newparam(:name) do
-    desc "The export name. Valid format is /vol/[volume_name](/[qtree_name])."
-    isnamevar
+  newparam(:name, :namevar => :true) do
+    desc "The export path. Valid format is /vol/[volume_name](/[qtree_name])."
     validate do |value|
     	unless value =~ /^(\/[\w]+){2,3}$/
-        	raise ArgumentError, "%s is not a valid export name." % value
-        end
+        raise ArgumentError, "%s is not a valid export name." % value
+      end
     end
   end
 
@@ -38,9 +37,93 @@ Puppet::Type.newtype(:netapp_export) do
     defaultto { @resource[:name] }
     validate do |value|
     	unless value =~ /^(\/[\w]+){2,3}$/
-        	raise ArgumentError, "%s is not a valid export filer path." % value
-        end
+        raise ArgumentError, "%s is not a valid export filer path." % value
+      end
     end
   end
   
+  newproperty(:anon) do 
+    desc "Should the export support anonymous root access."
+    defaultto '0'
+    validate do |value|
+      raise ArgumentError, "Anon should be a string." unless value.is_a?String
+    end
+    
+    def insync?(is)
+      # Should is an array, so pull first value.
+      should = @should.first
+
+      return false unless is == should
+
+      # Got here, so must match
+      return true
+
+    end
+  end
+  
+  newproperty(:readonly, :array_matching => :all) do
+    desc "Export read-only hosts."
+
+    def insync?(is)
+      # Check that is is an array
+      return false unless is.is_a? Array
+
+      # Check the first value to see if 'all_hosts'.
+      if is.first == 'all_hosts' && @should.first == 'all_hosts'
+        return true
+      else
+        # If they were different lengths, they are not equal.
+        return false unless is.length == @should.length
+
+        # Check that is and @should are the same...
+        return (is == @should or is == @should.map(&:to_s))
+
+      end
+    end
+
+    def should_to_s(newvalue)
+      newvalue.inspect
+    end
+
+    def is_to_s(currentvalue)
+      currentvalue.inspect
+    end
+  end
+
+  newproperty(:readwrite, :array_matching => :all) do
+    desc "Export read-write hosts. Defaults to 'all_hosts'."
+    defaultto ['all_hosts']
+    
+    def insync?(is)
+      # Check that is is an array
+      return false unless is.is_a? Array
+
+      # Check the first value to see if 'all_hosts'.
+      if is.first == 'all_hosts' && @should.first == 'all_hosts'
+        return true
+      else
+        Puppet.debug("Is and Should are hostname arrays... ")
+        # If they were different lengths, they are not equal.
+        return false unless is.length == @should.length
+
+        # Check that is and @should are the same...
+        return (is == @should or is == @should.map(&:to_s))
+
+      end
+    end
+
+    def should_to_s(newvalue)
+      newvalue.inspect
+    end
+
+    def is_to_s(currentvalue)
+      currentvalue.inspect
+    end
+  end
+  
+  # Make sure that ReadOnly and ReadWrite aren't the same values. 
+  validate do
+    raise ArgumentError, "Readonly and Readwrite params cannot be the same." if self[:readwrite] == self[:readonly]
+  end
+
 end
