35547885bce43851b5ed23cd2cb3f521aaad473d
Gavin Williams <fatmcgav@gmail.com>
2013-04-05 16:01:53 +0100

    (#17) Further fact updates to work with Foreman.

--- lib/puppet/util/network_device/netapp/facts.rb
+++ lib/puppet/util/network_device/netapp/facts.rb
@@ -20,14 +20,20 @@ class Puppet::Util::NetworkDevice::Netapp::Facts
     sys_version = result.child_get_string("version")
     
     # Add to facts hash
-    @facts['operatingsystem'] = sys_version 
+    @facts['version'] = sys_version 
       
     # Invoke "system-get-info" call to gather system information. 
     result = @transport.invoke("system-get-info")
     
     # Pull out system-info subset. 
     sys_info = result.child_get("system-info")
+
+    # Get DNS domainname to build up fqdn
+    result = @transport.invoke("options-get", "name", "dns.domainname")
+    domain_name = result.child_get_string("value")
+    @facts['domain'] = domain_name
     
+    # Array of values to get
     [ 'system-name',
       'system-id',
       'system-model',
@@ -46,13 +52,31 @@ class Puppet::Util::NetworkDevice::Netapp::Facts
     # cleanup of netapp output to match existing facter key values.
     map = { 'system-name'        => 'hostname',
             'memory-size'        => 'memorysize',
-            'system-model'       => 'hardwaremodel',
+            'system-model'       => 'productname',
             'cpu-processor-type' => 'processor',
     }
     @facts = Hash[@facts.map {|k, v| [map[k] || k, v] }]\
 
     # Need to replace '-' with '_'
     @facts = Hash[@facts.map {|k, v| [k.to_s.gsub('-','_'), v] }]
+
+    # Set operatingsystem details if present
+    if @facts['version'] then
+      if @facts['version'] =~ /^(NetApp) Release (\d.\d.\d\w+)/i
+        @facts['operatingsystem'] = $1
+        @facts['operatingsystemrelease'] = $2
+      end
+    end
+
+    # Handle FQDN
+    if @facts['hostname'].include? @facts['domain']
+      # Hostname contains the domain, therefore must be FQDN
+      @facts['fqdn'] = @facts['hostname']
+      @facts['hostname'] = @facts['fqdn'].split('.',1).shift
+    else 
+      # Hostname doesnt include domain. 
+      @facts['fqdn'] = @facts['hostname'] + '.' + @facts['domain']
+    end
 
     # Return array to calling class. 
     @facts
