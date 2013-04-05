68a2aeeb9b2288b4d7bb166c619bc7cbbdecdee1
Gavin Williams <fatmcgav@gmail.com>
2013-04-05 10:10:09 +0100

    (#17) First set of changes to device facts.

--- lib/puppet/util/network_device/netapp/facts.rb
+++ lib/puppet/util/network_device/netapp/facts.rb
@@ -13,6 +13,15 @@ class Puppet::Util::NetworkDevice::Netapp::Facts
     # Create empty array
     @facts = {}
     
+    # Invoke "system-get-version" to gather system version. 
+    result = @transport.invoke("system-get-version")
+    
+    # Pull out version
+    sys_version = result.child_get_string("version")
+    
+    # Add to facts hash
+    @facts['operatingsystem'] = sys_version 
+      
     # Invoke "system-get-info" call to gather system information. 
     result = @transport.invoke("system-get-info")
     
@@ -29,10 +38,22 @@ class Puppet::Util::NetworkDevice::Netapp::Facts
       'system-revision',
       'number-of-processors',
       'memory-size',
+      'cpu-processor-type'
       ].each do |key|
         @facts[key] = sys_info.child_get_string("#{key}".to_s)
     end
       
+    # cleanup of netapp output to match existing facter key values.
+    map = { 'system-name'        => 'hostname',
+            'memory-size'        => 'memorysize',
+            'system-model'       => 'hardwaremodel',
+            'cpu-processor-type' => 'processor',
+    }
+    @facts = Hash[@facts.map {|k, v| [map[k] || k, v] }]\
+
+    # Need to replace '-' with '_'
+    @facts = Hash[@facts.map {|k, v| [k.to_s.gsub('-','_'), v] }]
+
     # Return array to calling class. 
     @facts
     
