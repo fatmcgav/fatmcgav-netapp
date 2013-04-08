6bfe3412baa69880483a513447a74a259b04b21e
Gavin Williams <fatmcgav@gmail.com>
2013-04-08 14:12:42 +0100

    (#18) Updated instances and prefetch following changes to other types/providers in #20.

--- lib/puppet/provider/netapp_snapmirror/netapp_snapmirror.rb
+++ lib/puppet/provider/netapp_snapmirror/netapp_snapmirror.rb
@@ -6,18 +6,53 @@ Puppet::Type.type(:netapp_snapmirror).provide(:netapp_snapmirror, :parent => Pup
   confine :feature => :posix
   defaultfor :feature => :posix
   
+  mk_resource_methods
+  
   def self.instances
+    Puppet.debug("Puppet::Provider::Netapp_snapmirror: got to self.instances.")
+    
+    snapmirror_instances = Array.new
+    
+    # Query Netapp for snapmirror list. 
     result = transport.invoke('snapmirror-get-status')
+    # Check result status.
     if(result.results_status == "failed")
-      Puppet.debug("Puppet::Provider::Netapp_volume: Snapmirror-get-status failed. \n")
+      Puppet.debug("Puppet::Provider::Netapp_snapmirror: Snapmirror-get-status failed due to #{result.results_reason}. \n")
+      raise Puppet::Error, "Puppet::Device::Netapp_snapmirror: Snapmirror-get-status failed due to #{result.results_reason}. \n."
       return false
     else 
       # Pull list of snapmirror-status blocks
       snapmirror_list = result.child_get('snapmirror-status')
-      snapmirror_info = snapmirror_list.children_get()
-      snapmirror_info.each do |snapmirror|
+      snapmirror_instances = snapmirror_list.children_get()
+      
+      # Iterate array
+      snapmirror_instances.each do |snapmirror|
+        # Pull out the destination location value
         destination_location = snapmirror.child_get_string('destination-location')
-        new(:destination_location => destination_location)
+        Puppet.debug("Puppet::Provider::Netapp_snapmirror: Processing snapmirror relatioship for destination #{destination_location}.")
+        
+        # Create hash of information 
+        snapmirror_info = { :destination_location => destination_location,
+                            :ensure => :present }
+                            
+        # Create the instance and add to snapmirror instances array
+        Puppet.debug("Creating instance for '#{destination_location}.")
+        snapmirror_instances << new(snapmirror_info)
+      end
+      
+      # Return the final user array. 
+      Puppet.debug("Returning snapmirror array. ")
+      snapmirror_instances
+    end
+  end
+  
+  def self.prefetch(resources)
+    Puppet.debug("Puppet::Provider::Netapp_snapmirror: Got to self.prefetch.")
+    # Iterate instances and match provider where relevant.
+    instances.each do |prov|
+      Puppet.debug("Prov.name = #{resources[prov.name]}. ")
+      if resource = resources[prov.name]
+        resource.provider = prov
       end
     end
   end
@@ -64,45 +99,12 @@ Puppet::Type.type(:netapp_snapmirror).provide(:netapp_snapmirror, :parent => Pup
   
   def destroy
     Puppet.debug("Puppet::Provider::Netapp_snapmirror: destroying Netapp SnapMirror for Source #{@resource[:source_location]} to Destination #{@resource[:destination_location]}")
+    @property_hash[:ensure] = :absent
   end
   
   def exists?
-    Puppet.debug("Puppet::Provider::Netapp_snapmirror: checking status of SnapMirror for Source #{@resource[:source_location]} to Destination #{@resource[:destination_location]}")
-    
-    # Call webservice to list volume info
-    result = transport.invoke("snapmirror-get-status", "location", @resource[:destination_location])
-    Puppet.debug("Puppet::Provider::Netapp_snapmirror: Snapmirror status: " + result.sprintf() + "\n")
-    # Check response status. 
-    if(result.results_status == "failed")
-      Puppet.debug("Puppet::Provider::Netapp_snapmirror: Something went wrong checking for relationship... #{result.results_reason}. \n")
-      raise Puppet::Error, "Puppet::Device::Netapp_snapmirror something went wrong checking for relationship: #{result.results_reason} \n."
-      return false
-    else 
-      Puppet.debug("Puppet::Provider::Netapp_snapmirror: Checking if relationship exists... \n")
-      
-      sm_status = result.child_get('snapmirror-status')
-      if !sm_status
-        Puppet.debug("Puppet::Provider::Netapp_snapmirror: No relationship exist for #{@resource[:destination_location]}... \n")
-        return false
-      else 
-        # Should probably check to see if a relationship similar to what we're trying to create already exists?
-        relationships = sm_status.children_get
-        
-        # Itterate through the relationships for this source_location. 
-        relationships.each do |relationship|
-          source_location = relationship.child_get_string('source-location')
-          destination_location = relationship.child_get_string('destination-location')
-          
-          if (source_location == @resource[:source_location] && destination_location == @resource[:destination_location])
-            Puppet.debug("Puppet::Provider::Netapp_snapmirror: relationship already exists for source_location #{@resource[:source_location]} and destination_location #{@resource[:destination_location]}. ")
-            return true
-          end
-        end
-      end
-      
-      # Got through to here, therefore hasn't matched above. 
-      return false
-    end
+    Puppet.debug("Puppet::Provider::Netapp_snapmirror: checking existence of SnapMirror for Source #{@resource[:source_location]} to Destination #{@resource[:destination_location]}")
+    @property_hash[:ensure] == :present
   end
   
 end
