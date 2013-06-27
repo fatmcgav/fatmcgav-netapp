6fa5fe056a1a22ee7172a58876ef2d415a57995b
Stefan Schulte <stefan.schulte@taunusstein.net>
2013-06-27 23:14:16 +0200

    Fix netapp_spec and pass a correct url
    The netapp network device initialize method expects a NetApp filer and
    no URL. This is now respected in the spec test.
    
    Note: You might see some "Error : " messages messing the rspec output.
    This is because the NetApp SDK likes to talk about the expected
    exceptions.

--- lib/puppet/util/network_device/netapp/device.rb
+++ lib/puppet/util/network_device/netapp/device.rb
@@ -6,16 +6,16 @@ require 'yaml'
 class Puppet::Util::NetworkDevice::Netapp::Device
 
   attr_accessor :filer, :transport
+
+  def self.configfile
+    File.join(Puppet[:confdir], 'netapp.yml')
+  end
   
   def initialize(filer)
 
     Puppet.debug("Puppet::Device::Netapp: connecting to Netapp device #{filer}.")
-    #This should work to find the configdir
-    configdir = Puppet[:confdir]
-    Puppet.debug("Puppet::Device::Netapp: configdir is #{configdir}.")
 
-    configfile = File.read(configdir+"/netapp.yml")
-    filerconfig = YAML.load(configfile)[filer]
+    filerconfig = YAML.load_file(self.class.configfile)[filer]
     username = filerconfig[:user]
     password = filerconfig[:password]
     if(username == nil || password == nil)
@@ -32,12 +32,12 @@ class Puppet::Util::NetworkDevice::Netapp::Device
     result = @transport.invoke("system-get-version")
     if(result.results_errno() != 0)
       r = result.results_reason()
-      raise Puppet::Error, "Puppet::Device::Netapp: invoke system-get-version failed : \n #{r} \n"
+      raise Puppet::Error, "Puppet::Device::Netapp: invoke system-get-version failed: #{r}"
     else
       version = result.child_get_string("version")
       Puppet.debug("Puppet::Device::Netapp: Verion = #{version}")
     end
-   end
+  end
 		
   def facts
     @facts ||= Puppet::Util::NetworkDevice::Netapp::Facts.new(@transport)
