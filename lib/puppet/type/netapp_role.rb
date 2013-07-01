b3e7afdf859d9c042ef30f80ed3f78826e3c2881
Gavin Williams <fatmcgav@gmail.com>
2013-07-01 17:08:08 +0100

    (#39) Update netapp_role type and spec tests.

--- lib/puppet/type/netapp_role.rb
+++ lib/puppet/type/netapp_role.rb
@@ -3,19 +3,7 @@ Puppet::Type.newtype(:netapp_role) do
   
   apply_to_device
   
-  ensurable do
-    desc "Netapp Role resource state. Valid values are: present, absent."
-    
-    defaultto(:present)
-    
-    newvalue(:present) do 
-      provider.create
-    end
-    
-    newvalue(:absent) do 
-      provider.destroy
-    end
-  end
+  ensurable
   
   newparam(:rolename) do
     desc "The role name."
@@ -38,6 +26,12 @@ Puppet::Type.newtype(:netapp_role) do
   
   newproperty(:capabilities) do
     desc "List of capabilities for this role. Comma separate multiple values. "
+    
+    validate do |value|
+      unless value =~ /^[\w\s\-\*]+(,?[\w\s\-\*]*)*$/
+         raise ArgumentError, "%s is not a valid capabilities list." % value
+      end
+    end
     
     def insync?(is)
       # @should is an Array. see lib/puppet/type.rb insync?
--- spec/unit/puppet/type/netapp_role_spec.rb
+++ spec/unit/puppet/type/netapp_role_spec.rb
@@ -1,43 +1,98 @@
 require 'spec_helper'
  
-res_type_name = :netapp_role
-res_type = Puppet::Type.type(res_type_name)
-res_name = 'test'
+describe Puppet::Type.type(:netapp_role) do
 
-describe res_type do
-
-  let(:provider) {
-    prov = stub 'provider'
-    prov.stubs(:rolename).returns(res_type_name)
-    prov
-  }
-  let(:res_type) {
-    val = res_type
-    val.stubs(:defaultprovider).returns provider
-    val
-  }
-  let(:resource) {
-    res_type.new({:rolename => res_name})
-  }
-
-  it 'should have :rolename be its namevar' do
-    res_type.key_attributes.should == [:rolename]
+  before :each do
+    described_class.stubs(:defaultprovider).returns providerclass
   end
 
-  # Simple parameter tests
-  parameter_tests = {
-    :rolename => {
-      :valid    => ["test", "puppet"],
-      :invalid 	=> ["invalid role","test#"],
-    },
-    :comment => {
-      :valid    => ["This is a test comment.", "Valid comment."],
-      :invalid  => "# Invalid Comment",
-    },
-    :capabilities => {
-      :valid => ["cli-*","api-*,login-*"],
-    }
-  }
-  it_should_behave_like "a puppet type", parameter_tests, res_type_name, res_name
+  let :providerclass do
+    described_class.provide(:fake_netapp_role_provider) { mk_resource_methods }
+  end
+
+  it "should have :rolename be its namevar" do
+    described_class.key_attributes.should == [:rolename]
+  end
+
+  describe "when validating attributes" do
+    [:rolename, :provider, :comment].each do |param|
+      it "should have a #{param} parameter" do
+        described_class.attrtype(param).should == :param
+      end
+    end
+
+    [:ensure, :capabilities].each do |prop|
+      it "should have a #{prop} property" do
+        described_class.attrtype(prop).should == :property
+      end
+    end
+  end
+
+  describe "when validating values" do
+    describe "for rolename" do
+      it "should support an alphanumerical name" do
+        described_class.new(:rolename => 'role1', :ensure => :present)[:rolename].should == 'role1'
+      end
+
+      it "should support underscores" do
+        described_class.new(:rolename => 'role_1', :ensure => :present)[:rolename].should == 'role_1'
+      end
+
+      it "should support hyphens" do
+        described_class.new(:rolename => 'role-1', :ensure => :present)[:rolename].should == 'role-1'
+      end
+
+      it "should not support spaces" do
+        expect { described_class.new(:rolename => 'role 1', :ensure => :present) }.to raise_error(Puppet::Error, /role 1 is not a valid role name/)
+      end
+    end
+
+    describe "for ensure" do
+      it "should support present" do
+        described_class.new(:rolename => 'role1', :ensure => 'present')[:ensure].should == :present
+      end
+
+      it "should support absent" do
+        described_class.new(:rolename => 'role1', :ensure => 'absent')[:ensure].should == :absent
+      end
+
+      it "should not support other values" do
+        expect { described_class.new(:rolename => 'role1', :ensure => 'foo') }.to raise_error(Puppet::Error, /Invalid value "foo"/)
+      end
+      
+      it "should not have a default value" do
+        described_class.new(:rolename => 'role1')[:ensure].should == nil
+      end
+    end
+    
+    describe "for comment" do
+      it "should support an alphanumerical comment, with hyphens and fullstop" do
+        described_class.new(:rolename => 'role1', :comment => 'This is test role-1.', :ensure => :present)[:comment].should == 'This is test role-1.'
+      end
+
+      it "should not support special characters" do
+        expect { described_class.new(:rolename => 'role1', :comment => 'This is test role !', :ensure => :present) }.to raise_error(Puppet::Error, /This is test role ! is not a valid comment/)
+      end
+    end
+    
+    describe "for capabilities" do
+      it "should support a single capability" do
+        described_class.new(:rolename => 'role', :capabilities => 'login-api')[:capabilities].should == 'login-api'
+      end
+      
+      it "should support a comma-seperated list of capabilities" do
+        described_class.new(:rolename => 'role1', :capabilities => 'login-api,login-ssh')[:capabilities].should == 'login-api,login-ssh'
+      end
+      
+      it "should support a wildcard capability" do
+        described_class.new(:rolename => 'role1', :capabilities => 'login-*')[:capabilities].should == 'login-*'
+      end
+      
+      it "should not support special characters" do
+        expect { described_class.new(:rolename => 'user1', :capabilities => 'login-!') }.to raise_error(Puppet::Error, /login-! is not a valid capabilities list/)
+      end
+    end
+    
+  end
 
 end
