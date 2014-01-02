ea84605eb55e0f9767bd2de96466446fcb36d9fd
Santosh Kumar13 <Santosh_Kumar13@dell.com>
2014-01-02 11:31:05 +0000

    lun map/unmap & online/offline rspec code Reviewed by Vipin

---
+++ spec/unit/puppet/provider/netapp_lun_map_unmap/netapp_lun_map_unmap_spec.rb
@@ -0,0 +1,57 @@
+#! /usr/bin/env ruby
+
+require 'spec_helper'
+require 'yaml'
+
+
+describe Puppet::Type.type(:netapp_lun_map_unmap).provider(:netapp_lun_map_unmap) do
+
+  device_conf =  YAML.load_file(my_fixture('device_conf.yml'))
+  before :each do
+    Facter.stubs(:value).with(:url).returns(device_conf['url'])
+    described_class.stubs(:suitable?).returns true
+    Puppet::Type.type(:netapp_lun_map_unmap).stubs(:defaultprovider).returns described_class
+  end
+
+  let :lun_map do
+    Puppet::Type.type(:netapp_lun_map_unmap).new(
+      :name     => '/vol/testVolume/testLun1',
+      :ensure   => :present,
+      :initiatorgroup   => 'TestGroupNetApp'
+    )
+  end
+
+  let :lun_unmap do
+    Puppet::Type.type(:netapp_lun_map_unmap).new(
+      :name     => '/vol/testVolume/testLun1',
+      :ensure   => :present,
+      :initiatorgroup   => 'TestGroupNetApp'
+    )
+  end
+
+ 
+  let :provider do
+    described_class.new( )
+  end
+  
+  describe "when asking exists?" do
+    it "should return false if lun is not mapped" do
+      lun_map.provider.should_not be_exists
+    end
+  end
+
+  describe "when mapping a lun" do
+    it "should be able to map a lun" do
+      lun_map.provider.should_not be_exists
+      lun_map.provider.create
+    end
+  end
+
+  describe "when un-mapping a lun" do
+    it "should be able to un-map a lun" do
+      lun_unmap.provider.should be_exists
+      lun_unmap.provider.destroy
+    end
+  end
+
+end
---
+++ spec/unit/puppet/type/netapp_lun_online_offline_spec.rb
@@ -0,0 +1,57 @@
+#! /usr/bin/env ruby
+
+require 'spec_helper'
+
+describe Puppet::Type.type(:netapp_lun_online_offline) do
+
+  let :resource do
+    described_class.new(
+          :name          		=> '/vol/testVolume/testLun1',
+	  :ensure        		=> 'present',
+	  :force 		        => :true
+    )
+  end
+
+  
+  it "should have name as its keyattribute" do
+    described_class.key_attributes.should == [:name]
+  end
+
+  
+  describe "when validating attributes" do
+    [:name].each do |param|
+      it "should hava a #{param} parameter" do
+        described_class.attrtype(param).should == :param
+      end
+    end
+  end
+
+  
+  describe "when validating values" do
+  
+    describe "for name" do
+      it "should allow a valid mapping name where ensure is present" do
+        described_class.new(:name => '/vol/testVolume/testLun1', :ensure => 'present')[:name].should == '/vol/testVolume/testLun1'
+      end
+
+      it "should allow a valid mapping name where ensure is absent" do
+        described_class.new(:name => '/vol/testVolume/testLun1', :ensure => 'absent')[:name].should == '/vol/testVolume/testLun1'
+      end
+    end
+
+    describe "for ensure" do
+      it "should allow present" do
+        described_class.new(:name => '/vol/testVolume/testLun1', :ensure => 'present')[:ensure].should == :present
+      end
+
+      it "should allow absent" do
+        described_class.new(:name => '/vol/testVolume/testLun1', :ensure => 'absent')[:ensure].should == :absent
+      end
+
+      it "should not allow something else" do
+        expect { described_class.new(:name => 'newvolThree', :ensure => 'foo') }.to raise_error Puppet::Error, /Invalid value/
+      end
+    end
+	
+  end	
+end
