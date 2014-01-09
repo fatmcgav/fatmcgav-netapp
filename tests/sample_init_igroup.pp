f23c29f4c346f5497b20605ae3816cb926c86385
Santosh Kumar13 <Santosh_Kumar13@dell.com>
2014-01-09 05:14:47 +0000

    netapp code refactoring Reviewed by Vipin

---
+++ tests/sample_init_igroup.pp
@@ -0,0 +1,22 @@
+# Class: netapp
+#
+# This module manages netapp
+#
+# Parameters:
+#
+# Actions:
+#
+# Requires:
+#
+# Sample Usage:
+#
+# [Remember: No empty lines between comments and class definition]
+
+class netapp {
+  igroup { 'abcd':
+    ensure             => 'present',
+    initiatorgrouptype => 'fcp',
+    ostype             => 'linux',
+  }
+}
+
