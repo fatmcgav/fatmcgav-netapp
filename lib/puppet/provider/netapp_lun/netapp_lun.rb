d75d1b98696b4484f0258afa695c61374fea3f02
Santosh Kumar13 <Santosh_Kumar13@dell.com>
2014-01-13 07:27:32 +0000

    netapp code refactoring Reviewed by Vipin

--- lib/puppet/provider/netapp_lun/netapp_lun.rb
+++ lib/puppet/provider/netapp_lun/netapp_lun.rb
@@ -56,38 +56,30 @@ Puppet::Type.type(:netapp_lun).provide(:netapp_lun, :parent => Puppet::Provider:
   def create
     Puppet.debug("Inside create method.")
     Puppet.info("Creating LUN '#{@resource[:name]}'")
+    exitvalue = luncreate(*get_create_command)
+    #Puppet.debug("Current LUN size after executing create operation - #{exitvalue.child_get_int("actual-size")}")
     lun_exists = get_lun_existence_status
-    if  "#{lun_exists}" == "false"
-      exitvalue = luncreate(*get_create_command)
-      Puppet.debug("Current LUN size after executing create operation - #{exitvalue.child_get_int("actual-size")}")
-      lun_exists = get_lun_existence_status
-      if  "#{lun_exists}" == "true"
-        Puppet.info("LUN '#{@resource[:name]}' created successfully.")
-      else
-        raise Puppet::Error, "Failed to create the LUN '@resource[:name]'"
-      end
+    Puppet.debug("Lun existence status after executing create operation - #{lun_exists}")
+    if  "#{lun_exists}" == "true"
+      Puppet.info("LUN '#{@resource[:name]}' created successfully.")
     else
-      Puppet.info("LUN '#{@resource[:name]}' already exists.")
+      raise Puppet::Error, "Failed to create the LUN '@resource[:name]'"
     end
   end
 
   def destroy
     Puppet.debug("Inside destroy method.")
     Puppet.info("Destroying LUN '#{@resource[:name]}'")
+    lundestroy(*get_destroy_command)
     lun_exists = get_lun_existence_status
-    if  "#{lun_exists}" == "true"
-      lundestroy(*get_destroy_command)
-      lun_exists = get_lun_existence_status
-      Puppet.debug("Lun existence status after executing destroy operation - #{lun_exists}")
-      if  "#{lun_exists}" == "false"
-        Puppet.info("Successfully destroyed the LUN '#{@resource[:name]}'")
-      else
-        #Puppet.info("Failed to destroy the LUN '@resource[:name]'")
-        raise Puppet::Error, "Failed to destroy the LUN '#{@resource[:name]}'"
-      end
+    Puppet.debug("Lun existence status after executing destroy operation - #{lun_exists}")
+    if  "#{lun_exists}" == "false"
+      Puppet.info("Successfully destroyed the LUN '#{@resource[:name]}'")
     else
-      Puppet.info("LUN '@resource[:name]' does not exists.")
+      #Puppet.info("Failed to destroy the LUN '@resource[:name]'")
+      raise Puppet::Error, "Failed to destroy the LUN '#{@resource[:name]}'"
     end
+
   end
 
   def exists?
@@ -105,4 +97,3 @@ Puppet::Type.type(:netapp_lun).provide(:netapp_lun, :parent => Puppet::Provider:
   end
 
 end
-
