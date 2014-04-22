d2da3a4e0653b6c79301ef1ccb6749b40c7c6812
Gavin Williams <gavin.williams@weareact.com>
2014-04-22 09:40:36 +0100

    (#7) Use described_class name.

--- spec/unit/puppet/type/netapp_role_spec.rb
+++ spec/unit/puppet/type/netapp_role_spec.rb
@@ -8,8 +8,8 @@ describe Puppet::Type.type(:netapp_role) do
       :comment      => 'Role comment', 
       :capabilities => 'capability'
     }
-    @provider = stub('provider', :class => Puppet::Type.type(:netapp_role).defaultprovider, :clear => nil)
-    Puppet::Type.type(:netapp_role).defaultprovider.stubs(:new).returns(@provider)
+    @provider = stub('provider', :class => described_class.defaultprovider, :clear => nil)
+    described_class.defaultprovider.stubs(:new).returns(@provider)
   end
 
   let :role_resource do 
@@ -17,19 +17,19 @@ describe Puppet::Type.type(:netapp_role) do
   end
 
   it "should have :rolename be its namevar" do
-    Puppet::Type.type(:netapp_role).key_attributes.should == [:rolename]
+    described_class.key_attributes.should == [:rolename]
   end
 
   describe "when validating attributes" do
     [:rolename, :provider, :comment].each do |param|
       it "should have a #{param} parameter" do
-        Puppet::Type.type(:netapp_role).attrtype(param).should == :param
+        described_class.attrtype(param).should == :param
       end
     end
 
     [:ensure, :capabilities].each do |prop|
       it "should have a #{prop} property" do
-        Puppet::Type.type(:netapp_role).attrtype(prop).should == :property
+        described_class.attrtype(prop).should == :property
       end
     end
   end
@@ -37,79 +37,79 @@ describe Puppet::Type.type(:netapp_role) do
   describe "when validating values" do
     describe "for rolename" do
       it "should support an alphanumerical name" do
-        Puppet::Type.type(:netapp_role).new(:rolename => 'role1', :ensure => :present)[:rolename].should == 'role1'
+        described_class.new(:rolename => 'role1', :ensure => :present)[:rolename].should == 'role1'
       end
 
       it "should support underscores" do
-        Puppet::Type.type(:netapp_role).new(:rolename => 'role_1', :ensure => :present)[:rolename].should == 'role_1'
+        described_class.new(:rolename => 'role_1', :ensure => :present)[:rolename].should == 'role_1'
       end
 
       it "should support hyphens" do
-        Puppet::Type.type(:netapp_role).new(:rolename => 'role-1', :ensure => :present)[:rolename].should == 'role-1'
+        described_class.new(:rolename => 'role-1', :ensure => :present)[:rolename].should == 'role-1'
       end
 
       it "should not support spaces" do
-        expect { Puppet::Type.type(:netapp_role).new(:rolename => 'role 1', :ensure => :present) }.to raise_error(Puppet::Error, /role 1 is not a valid role name/)
+        expect { described_class.new(:rolename => 'role 1', :ensure => :present) }.to raise_error(Puppet::Error, /role 1 is not a valid role name/)
       end
     end
 
     describe "for ensure" do
       it "should support present" do
-        Puppet::Type.type(:netapp_role).new(:rolename => 'role1', :ensure => 'present')[:ensure].should == :present
+        described_class.new(:rolename => 'role1', :ensure => 'present')[:ensure].should == :present
       end
 
       it "should support absent" do
-        Puppet::Type.type(:netapp_role).new(:rolename => 'role1', :ensure => 'absent')[:ensure].should == :absent
+        described_class.new(:rolename => 'role1', :ensure => 'absent')[:ensure].should == :absent
       end
 
       it "should not support other values" do
-        expect { Puppet::Type.type(:netapp_role).new(:rolename => 'role1', :ensure => 'foo') }.to raise_error(Puppet::Error, /Invalid value "foo"/)
+        expect { described_class.new(:rolename => 'role1', :ensure => 'foo') }.to raise_error(Puppet::Error, /Invalid value "foo"/)
       end
       
       it "should not have a default value" do
-        Puppet::Type.type(:netapp_role).new(:rolename => 'role1')[:ensure].should == nil
+        described_class.new(:rolename => 'role1')[:ensure].should == nil
       end
     end
     
     describe "for comment" do
       it "should support an alphanumerical comment, with hyphens and fullstop" do
-        Puppet::Type.type(:netapp_role).new(:rolename => 'role1', :comment => 'This is test role-1.', :ensure => :present)[:comment].should == 'This is test role-1.'
+        described_class.new(:rolename => 'role1', :comment => 'This is test role-1.', :ensure => :present)[:comment].should == 'This is test role-1.'
       end
 
       it "should not support special characters" do
-        expect { Puppet::Type.type(:netapp_role).new(:rolename => 'role1', :comment => 'This is test role !', :ensure => :present) }.to raise_error(Puppet::Error, /This is test role ! is not a valid comment/)
+        expect { described_class.new(:rolename => 'role1', :comment => 'This is test role !', :ensure => :present) }.to raise_error(Puppet::Error, /This is test role ! is not a valid comment/)
       end
     end
     
     describe "for capabilities" do
       it "should support a single capability" do
-        Puppet::Type.type(:netapp_role).new(:rolename => 'role', :capabilities => 'login-api')[:capabilities].should == 'login-api'
+        described_class.new(:rolename => 'role', :capabilities => 'login-api')[:capabilities].should == 'login-api'
       end
       
       it "should support a comma-seperated list of capabilities" do
-        Puppet::Type.type(:netapp_role).new(:rolename => 'role1', :capabilities => 'login-api,login-ssh')[:capabilities].should == 'login-api,login-ssh'
+        described_class.new(:rolename => 'role1', :capabilities => 'login-api,login-ssh')[:capabilities].should == 'login-api,login-ssh'
       end
       
       it "should support a wildcard capability" do
-        Puppet::Type.type(:netapp_role).new(:rolename => 'role1', :capabilities => 'login-*')[:capabilities].should == 'login-*'
+        described_class.new(:rolename => 'role1', :capabilities => 'login-*')[:capabilities].should == 'login-*'
       end
       
       it "should not support special characters" do
-        expect { Puppet::Type.type(:netapp_role).new(:rolename => 'user1', :capabilities => 'login-!') }.to raise_error(Puppet::Error, /login-! is not a valid capabilities list/)
+        expect { described_class.new(:rolename => 'user1', :capabilities => 'login-!') }.to raise_error(Puppet::Error, /login-! is not a valid capabilities list/)
       end
       
       it "insync? should return false if is and should values dont match" do
         role = role_resource.dup
         is_capabilities = 'capability1'
         role[:capabilities] = 'capability1,capability2'
-        Puppet::Type.type(:netapp_role).new(role).property(:capabilities).insync?(is_capabilities).should be_false
+        described_class.new(role).property(:capabilities).insync?(is_capabilities).should be_false
       end
       
       it "insync? should return true if is and should values match" do
         role = role_resource.dup
         is_capabilities = 'capability1,capability2'
         role[:capabilities] = 'capability1,capability2'
-        Puppet::Type.type(:netapp_role).new(role).property(:capabilities).insync?(is_capabilities).should be_true
+        described_class.new(role).property(:capabilities).insync?(is_capabilities).should be_true
       end
     end
     
--- spec/unit/puppet/type/netapp_volume_spec.rb
+++ spec/unit/puppet/type/netapp_volume_spec.rb
@@ -10,8 +10,8 @@ describe Puppet::Type.type(:netapp_volume) do
       :aggregate   => 'aggr0', 
       :snapreserve => '0'
     }
-    @provider = stub('provider', :class => Puppet::Type.type(:netapp_volume).defaultprovider, :clear => nil)
-    Puppet::Type.type(:netapp_volume).defaultprovider.stubs(:new).returns(@provider)
+    @provider = stub('provider', :class => described_class.defaultprovider, :clear => nil)
+    described_class.defaultprovider.stubs(:new).returns(@provider)
   end
 
   let :volume_resource do 
@@ -19,19 +19,19 @@ describe Puppet::Type.type(:netapp_volume) do
   end
 
   it "should have :name be its namevar" do
-    Puppet::Type.type(:netapp_volume).key_attributes.should == [:name]
+    described_class.key_attributes.should == [:name]
   end
 
   describe "when validating attributes" do
     [:name, :provider, :aggregate, :languagecode, :spaceres].each do |param|
       it "should have a #{param} parameter" do
-        Puppet::Type.type(:netapp_volume).attrtype(param).should == :param
+        described_class.attrtype(param).should == :param
       end
     end
 
     [:ensure, :initsize, :snapreserve, :autoincrement, :options, :snapschedule].each do |prop|
       it "should have a #{prop} property" do
-        Puppet::Type.type(:netapp_volume).attrtype(prop).should == :property
+        described_class.attrtype(prop).should == :property
       end
     end
   end
@@ -39,215 +39,215 @@ describe Puppet::Type.type(:netapp_volume) do
   describe "when validating values" do
     describe "for name" do
       it "should support an alphanumerical name" do
-        Puppet::Type.type(:netapp_volume).new(:name => 'volume1', :ensure => :present)[:name].should == 'volume1'
+        described_class.new(:name => 'volume1', :ensure => :present)[:name].should == 'volume1'
       end
 
       it "should support underscores" do
-        Puppet::Type.type(:netapp_volume).new(:name => 'volume_1', :ensure => :present)[:name].should == 'volume_1'
+        described_class.new(:name => 'volume_1', :ensure => :present)[:name].should == 'volume_1'
       end
 
       it "should not support hyphens" do
-        expect { Puppet::Type.type(:netapp_volume).new(:name => 'volume-1', :ensure => :present) }.to raise_error(Puppet::Error, /volume-1 is not a valid volume name./)
+        expect { described_class.new(:name => 'volume-1', :ensure => :present) }.to raise_error(Puppet::Error, /volume-1 is not a valid volume name./)
       end
 
       it "should not support spaces" do
-        expect { Puppet::Type.type(:netapp_volume).new(:name => 'volume 1', :ensure => :present) }.to raise_error(Puppet::Error, /volume 1 is not a valid volume name/)
+        expect { described_class.new(:name => 'volume 1', :ensure => :present) }.to raise_error(Puppet::Error, /volume 1 is not a valid volume name/)
       end
     end
 
     describe "for ensure" do
       it "should support present" do
-        Puppet::Type.type(:netapp_volume).new(:name => 'volume', :ensure => 'present')[:ensure].should == :present
+        described_class.new(:name => 'volume', :ensure => 'present')[:ensure].should == :present
       end
 
       it "should support absent" do
-        Puppet::Type.type(:netapp_volume).new(:name => 'volume', :ensure => 'absent')[:ensure].should == :absent
+        described_class.new(:name => 'volume', :ensure => 'absent')[:ensure].should == :absent
       end
 
       it "should not support other values" do
-        expect { Puppet::Type.type(:netapp_volume).new(:name => 'volume', :ensure => 'foo') }.to raise_error(Puppet::Error, /Invalid value "foo"/)
+        expect { described_class.new(:name => 'volume', :ensure => 'foo') }.to raise_error(Puppet::Error, /Invalid value "foo"/)
       end
     end
 
     describe "for initsize" do
       it "should support a valid volume size" do
-        Puppet::Type.type(:netapp_volume).new(:name => 'volume', :initsize => '1g', :ensure => :present)[:initsize].should == '1g'
+        described_class.new(:name => 'volume', :initsize => '1g', :ensure => :present)[:initsize].should == '1g'
       end
 
       it "should not support spaces" do
-        expect { Puppet::Type.type(:netapp_volume).new(:name => 'volume', :initsize => '1 g', :ensure => :present) }.to raise_error(Puppet::Error, /1 g is not a valid initial volume size./)
+        expect { described_class.new(:name => 'volume', :initsize => '1 g', :ensure => :present) }.to raise_error(Puppet::Error, /1 g is not a valid initial volume size./)
       end
       
       it "should support have a default value of '1g'" do
-        Puppet::Type.type(:netapp_volume).new(:name => 'volume', :ensure => :present)[:initsize].should == '1g'
+        described_class.new(:name => 'volume', :ensure => :present)[:initsize].should == '1g'
       end
     end
     
     describe "for aggregate" do
       it "should support a valid aggregate name" do
-        Puppet::Type.type(:netapp_volume).new(:name => 'volume', :aggregate => 'aggr1', :ensure => :present)[:aggregate].should == 'aggr1'
+        described_class.new(:name => 'volume', :aggregate => 'aggr1', :ensure => :present)[:aggregate].should == 'aggr1'
       end
 
       it "should not support spaces" do
-        expect { Puppet::Type.type(:netapp_volume).new(:name => 'volume', :aggregate => 'aggregate 1', :ensure => :present) }.to raise_error(Puppet::Error, /aggregate 1 is not a valid aggregate name./)
+        expect { described_class.new(:name => 'volume', :aggregate => 'aggregate 1', :ensure => :present) }.to raise_error(Puppet::Error, /aggregate 1 is not a valid aggregate name./)
       end
     end
 
     describe "for languagecode" do
       it "should support a valid language code" do
-        Puppet::Type.type(:netapp_volume).new(:name => 'volume', :languagecode => 'en_US', :ensure => :present)[:languagecode].should == :en_US
+        described_class.new(:name => 'volume', :languagecode => 'en_US', :ensure => :present)[:languagecode].should == :en_US
       end
 
       it "should not support an invalid language code" do
-        expect { Puppet::Type.type(:netapp_volume).new(:name => 'volume', :languagecode => 'na', :ensure => :present) }.to raise_error(Puppet::Error, /Invalid value "na"/)
+        expect { described_class.new(:name => 'volume', :languagecode => 'na', :ensure => :present) }.to raise_error(Puppet::Error, /Invalid value "na"/)
       end
       
       it "should have a default value of 'en'" do
-        Puppet::Type.type(:netapp_volume).new(:name => 'volume', :ensure => :present)[:languagecode].should == :en
+        described_class.new(:name => 'volume', :ensure => :present)[:languagecode].should == :en
       end
     end
     
     describe "for spaceres" do
       it "should support none" do
-        Puppet::Type.type(:netapp_volume).new(:name => 'volume', :spaceres => 'none', :ensure => :present)[:spaceres].should == :none
+        described_class.new(:name => 'volume', :spaceres => 'none', :ensure => :present)[:spaceres].should == :none
       end
 
       it "should support file" do
-        Puppet::Type.type(:netapp_volume).new(:name => 'volume', :spaceres => 'file', :ensure => :present)[:spaceres].should == :file
+        described_class.new(:name => 'volume', :spaceres => 'file', :ensure => :present)[:spaceres].should == :file
       end
 
       it "should support volume" do
-        Puppet::Type.type(:netapp_volume).new(:name => 'volume', :spaceres => 'volume', :ensure => :present)[:spaceres].should == :volume
+        described_class.new(:name => 'volume', :spaceres => 'volume', :ensure => :present)[:spaceres].should == :volume
       end
             
       it "should not support an invalid value" do
-        expect { Puppet::Type.type(:netapp_volume).new(:name => 'volume', :spaceres => 'invalid', :ensure => :present) }.to raise_error(Puppet::Error, /Invalid value "invalid"/)
+        expect { described_class.new(:name => 'volume', :spaceres => 'invalid', :ensure => :present) }.to raise_error(Puppet::Error, /Invalid value "invalid"/)
       end
       
       it "should have a default value of 'none'" do
-        Puppet::Type.type(:netapp_volume).new(:name => 'volume', :ensure => :present)[:spaceres].should == :none
+        described_class.new(:name => 'volume', :ensure => :present)[:spaceres].should == :none
       end
     end
     
     describe "for snapreserve" do
       it "should support a number" do
-        Puppet::Type.type(:netapp_volume).new(:name => 'volume', :snapreserve => '20', :ensure => :present)[:snapreserve].should == 20
+        described_class.new(:name => 'volume', :snapreserve => '20', :ensure => :present)[:snapreserve].should == 20
       end
       
       it "should support 0" do
-        Puppet::Type.type(:netapp_volume).new(:name => 'volume', :snapreserve => '0', :ensure => :present)[:snapreserve].should == 0
+        described_class.new(:name => 'volume', :snapreserve => '0', :ensure => :present)[:snapreserve].should == 0
       end
       
       it "should support 100" do
-        Puppet::Type.type(:netapp_volume).new(:name => 'volume', :snapreserve => '100', :ensure => :present)[:snapreserve].should == 100
+        described_class.new(:name => 'volume', :snapreserve => '100', :ensure => :present)[:snapreserve].should == 100
       end
 
       it "should not support a negative number" do
-        expect { Puppet::Type.type(:netapp_volume).new(:name => 'volume', :snapreserve => '-20', :ensure => :present) }.to raise_error(Puppet::Error, /-20 is not a valid snapreserve./)
+        expect { described_class.new(:name => 'volume', :snapreserve => '-20', :ensure => :present) }.to raise_error(Puppet::Error, /-20 is not a valid snapreserve./)
       end
             
       it "should not support a non-numeric value" do
-        expect { Puppet::Type.type(:netapp_volume).new(:name => 'volume', :snapreserve => 'invalid', :ensure => :present) }.to raise_error(Puppet::Error, /invalid is not a valid snapreserve./)
+        expect { described_class.new(:name => 'volume', :snapreserve => 'invalid', :ensure => :present) }.to raise_error(Puppet::Error, /invalid is not a valid snapreserve./)
       end
       
       it "should not support a number greater than 100" do
-        expect { Puppet::Type.type(:netapp_volume).new(:name => 'volume', :snapreserve => '101', :ensure => :present) }.to raise_error(Puppet::Error, /Reserved percentage must be between 0 and 100./)
+        expect { described_class.new(:name => 'volume', :snapreserve => '101', :ensure => :present) }.to raise_error(Puppet::Error, /Reserved percentage must be between 0 and 100./)
       end
       
       it "should not have a default value" do
-        Puppet::Type.type(:netapp_volume).new(:name => 'volume', :ensure => :present)[:snapreserve].should == nil
+        described_class.new(:name => 'volume', :ensure => :present)[:snapreserve].should == nil
       end
     end
     
     describe "for autoincrement" do
       it "should support true" do
-        Puppet::Type.type(:netapp_volume).new(:name => 'volume', :autoincrement => true, :ensure => :present)[:autoincrement].should == :true
+        described_class.new(:name => 'volume', :autoincrement => true, :ensure => :present)[:autoincrement].should == :true
       end
       
       it "should support false" do
-        Puppet::Type.type(:netapp_volume).new(:name => 'volume', :autoincrement => false, :ensure => :present)[:autoincrement].should == :false
+        described_class.new(:name => 'volume', :autoincrement => false, :ensure => :present)[:autoincrement].should == :false
       end
       
       it "should have a default value of 'true'" do
-        Puppet::Type.type(:netapp_volume).new(:name => 'volume', :ensure => :present)[:autoincrement].should == :true
+        described_class.new(:name => 'volume', :ensure => :present)[:autoincrement].should == :true
       end
     end 
     
     describe "for options" do
       it "should support a hash" do
-        Puppet::Type.type(:netapp_volume).new(:name => 'volume', :options => {'hash' => 'yes'}, :ensure => :present)[:options][0].should == {"hash"=>"yes"}
+        described_class.new(:name => 'volume', :options => {'hash' => 'yes'}, :ensure => :present)[:options][0].should == {"hash"=>"yes"}
       end
       
       it "should not support an array" do
-        expect { Puppet::Type.type(:netapp_volume).new(:name => 'volume', :options => ['array'], :ensure => :present) }.to raise_error(Puppet::Error, /options property must be a hash./)
+        expect { described_class.new(:name => 'volume', :options => ['array'], :ensure => :present) }.to raise_error(Puppet::Error, /options property must be a hash./)
       end
       
       it "should not support a  string" do
-        expect { Puppet::Type.type(:netapp_volume).new(:name => 'volume', :options => 'string', :ensure => :present) }.to raise_error(Puppet::Error, /options property must be a hash./)
+        expect { described_class.new(:name => 'volume', :options => 'string', :ensure => :present) }.to raise_error(Puppet::Error, /options property must be a hash./)
       end
       
       it "should not have a default value" do
-        Puppet::Type.type(:netapp_volume).new(:name => 'volume', :ensure => :present)[:options].should == nil
+        described_class.new(:name => 'volume', :ensure => :present)[:options].should == nil
       end
       
       it "insync? should return false if is isn't a hash" do
         volume = volume_resource.dup
         is_options = 'option'
         volume[:options] = {'option' => 'value'}
-        Puppet::Type.type(:netapp_volume).new(volume).property(:options).insync?(is_options).should be_false
+        described_class.new(volume).property(:options).insync?(is_options).should be_false
       end
       
       it "insync? should return false if is and should don't match" do
         volume = volume_resource.dup
         is_options = {'option1' => 'value1'}
         volume[:options] = {'option2' => 'value2'}
-        Puppet::Type.type(:netapp_volume).new(volume).property(:options).insync?(is_options).should be_false
+        described_class.new(volume).property(:options).insync?(is_options).should be_false
       end
       
       it "insync? should return true if is and should match" do
         volume = volume_resource.dup
         is_options = {'option1' => 'value1'}
         volume[:options] = {'option1' => 'value1'}
-        Puppet::Type.type(:netapp_volume).new(volume).property(:options).insync?(is_options).should be_true
+        described_class.new(volume).property(:options).insync?(is_options).should be_true
       end
     end 
     
     describe "for snapschedule" do
       it "should support a hash" do
-        Puppet::Type.type(:netapp_volume).new(:name => 'volume', :snapschedule => {'hash' => 'yes'}, :ensure => :present)[:snapschedule][0].should == {"hash"=>"yes"}
+        described_class.new(:name => 'volume', :snapschedule => {'hash' => 'yes'}, :ensure => :present)[:snapschedule][0].should == {"hash"=>"yes"}
       end
       
       it "should not support an array" do
-        expect { Puppet::Type.type(:netapp_volume).new(:name => 'volume', :snapschedule => ['array'], :ensure => :present) }.to raise_error(Puppet::Error, /snapschedule property must be a hash./)
+        expect { described_class.new(:name => 'volume', :snapschedule => ['array'], :ensure => :present) }.to raise_error(Puppet::Error, /snapschedule property must be a hash./)
       end
       
       it "should not support a  string" do
-        expect { Puppet::Type.type(:netapp_volume).new(:name => 'volume', :snapschedule => 'string', :ensure => :present) }.to raise_error(Puppet::Error, /snapschedule property must be a hash./)
+        expect { described_class.new(:name => 'volume', :snapschedule => 'string', :ensure => :present) }.to raise_error(Puppet::Error, /snapschedule property must be a hash./)
       end
       
       it "should not have a default value" do
-        Puppet::Type.type(:netapp_volume).new(:name => 'volume', :ensure => :present)[:snapschedule].should == nil
+        described_class.new(:name => 'volume', :ensure => :present)[:snapschedule].should == nil
       end
       
       it "insync? should return false if is isn't a hash" do
         volume = volume_resource.dup
         is_snapsched = 'snapschedule'
         volume[:snapschedule] = {'minutes' => 'value', 'hours' => 'value', 'days' => 'value'}
-        Puppet::Type.type(:netapp_volume).new(volume).property(:snapschedule).insync?(is_snapsched).should be_false
+        described_class.new(volume).property(:snapschedule).insync?(is_snapsched).should be_false
       end
       
       it "insync? should return false if is and should don't match" do
         volume = volume_resource.dup
         is_snapsched = {'minutes' => 'value2', 'hours' => 'value', 'days' => 'value'}
         volume[:snapschedule] = {'minutes' => 'value', 'hours' => 'value', 'days' => 'value'}
-        Puppet::Type.type(:netapp_volume).new(volume).property(:snapschedule).insync?(is_snapsched).should be_false
+        described_class.new(volume).property(:snapschedule).insync?(is_snapsched).should be_false
       end
       
       it "insync? should return true if is and should match" do
         volume = volume_resource.dup
         is_snapsched = {'minutes' => 'value', 'hours' => 'value', 'days' => 'value'}
         volume[:snapschedule] = {'minutes' => 'value', 'hours' => 'value', 'days' => 'value'}
-        Puppet::Type.type(:netapp_volume).new(volume).property(:snapschedule).insync?(is_snapsched).should be_true
+        described_class.new(volume).property(:snapschedule).insync?(is_snapsched).should be_true
       end
     end 
   end
